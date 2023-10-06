c0ec9c81e6f7d2de581db8d238cf9577e01f652d
fredericoendres <frederico@eyemobile.com.br>
2023-10-06 17:09:01 -0300

    botao testar nav

--- app/src/main/java/devandroid/frederico/cafexyz/ui/home/adapter/HomeFragment.java
+++ app/src/main/java/devandroid/frederico/cafexyz/ui/home/adapter/HomeFragment.java
@@ -2,20 +2,28 @@ package devandroid.frederico.cafexyz.ui.home.adapter;
 
 import android.os.Bundle;
 
+import androidx.annotation.IdRes;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import java.util.ArrayList;
 
 import devandroid.frederico.cafexyz.R;
 import devandroid.frederico.cafexyz.data.ProductModel;
+import devandroid.frederico.cafexyz.ui.MainActivity;
 
 
 public class HomeFragment extends Fragment {
+
+    private NavController navController;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -32,6 +40,16 @@ public class HomeFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.home_fragment, container, false);
         RecyclerView productRecycle = view.findViewById(R.id.product_recycle);
+
+
+        Button button = view.findViewById(R.id.buttonhome);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Navigation.findNavController(requireActivity(), R.id.paymentFragment);
+            }
+        });
+
 
         ArrayList<ProductModel> productModelArrayList = new ArrayList<ProductModel>();
         productModelArrayList.add(new ProductModel("Ovos com bacon", "R$ 25,00", R.drawable.ovos));
---
+++ app/src/main/java/devandroid/frederico/cafexyz/ui/payment/PaymentFragment.java
@@ -0,0 +1,37 @@
+package devandroid.frederico.cafexyz.ui.payment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+import devandroid.frederico.cafexyz.R;
+import devandroid.frederico.cafexyz.data.ProductModel;
+import devandroid.frederico.cafexyz.ui.home.adapter.CardAdapter;
+
+
+public class PaymentFragment extends Fragment {
+
+    public PaymentFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.payment_layout, container, false);
+
+        return view;
+    }
+}
