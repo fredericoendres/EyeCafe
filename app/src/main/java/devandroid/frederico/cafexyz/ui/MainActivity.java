926ebb59dcf65a849e71cbd552761d0bb0a103ff
fredericoendres <116963749+fredericoendres@users.noreply.github.com>
2023-10-13 10:03:54 -0300

    Alterações no tema e animações

--- app/src/main/java/devandroid/frederico/cafexyz/ui/MainActivity.java
+++ app/src/main/java/devandroid/frederico/cafexyz/ui/MainActivity.java
@@ -10,8 +10,13 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 
 import java.util.ArrayList;
@@ -28,16 +33,43 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        /*if (Build.VERSION.SDK_INT >= 21) {
+            Window window = this.getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.setStatusBarColor(this.getResources().getColor(R.color.marrom));
+        }*/
 
         ImageView nextButton = findViewById(R.id.arrowBottom);
+        ImageView nextButton2 = findViewById(R.id.arrowBottom2);
+        View bottomBar = findViewById(R.id.bottomBar);
+        View bottomBar2 = findViewById(R.id.bottomBar2);
+        Animation fadeIn = AnimationUtils.loadAnimation(MainActivity.this, R.anim.fade_in);
+        Animation fadeOut = AnimationUtils.loadAnimation(MainActivity.this, R.anim.fade_out);
+
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.fragmentMain);
+        NavController navController = navHostFragment.getNavController();
 
         nextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.fragmentMain);
-                NavController navController = navHostFragment.getNavController();
-                navController.navigate(R.id.paymentFragment);
+                navController.navigate(R.id.cartFragment);
 
+
+
+                bottomBar.startAnimation(fadeOut);
+                bottomBar2.startAnimation(fadeIn);
+                bottomBar.setVisibility(View.GONE);
+                bottomBar2.setVisibility(View.VISIBLE);
+            }
+        });
+
+        nextButton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                navController.navigate(R.id.paymentFragment);
+                bottomBar2.startAnimation(fadeOut);
+                bottomBar2.setVisibility(View.GONE);
             }
         });
 
